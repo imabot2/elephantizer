e21687f70a08cfbb2b0ce27366c8d7f41481c8a5
Philippe Lucidarme <philippe@lucidar.me>
2023-11-26 19:01:24 +0100

    Add multiplication tables

--- src/catalog/fr/tables-de-multiplication/series.js
+++ src/catalog/fr/tables-de-multiplication/series.js
@@ -10,6 +10,22 @@ const series = {
     name: translate.tableDe1,
     shortName: translate.tableDe1Short,
   },
+  "table-de-2" : {
+    name: translate.tableDe2,
+    shortName: translate.tableDe2Short,
+  },
+  "table-de-3" : {
+    name: translate.tableDe3,
+    shortName: translate.tableDe3Short,
+  },
+  "table-de-4" : {
+    name: translate.tableDe4,
+    shortName: translate.tableDe4Short,
+  },
+  "table-de-5" : {
+    name: translate.tableDe5,
+    shortName: translate.tableDe5Short,
+  },
 };
 
 
--- src/catalog/fr/tables-de-multiplication/translate.js
+++ src/catalog/fr/tables-de-multiplication/translate.js
@@ -10,6 +10,15 @@ const translations = {
     tableDe0Short: 'O Times Table',
     tableDe1: '1 Times Multiplication Table',
     tableDe1Short: '1 Times Table',
+    tableDe2: '2 Times Multiplication Table',
+    tableDe2Short: '2 Times Table',
+    tableDe3: '3 Times Multiplication Table',
+    tableDe3Short: '3 Times Table',
+    tableDe4: '4 Times Multiplication Table',
+    tableDe4Short: '4 Times Table',
+    tableDe5: '5 Times Multiplication Table',
+    tableDe5Short: '5 Times Table',
+    
   },
 
   fr: {
@@ -17,6 +26,16 @@ const translations = {
     tableDe0Short: 'Table de 0',
     tableDe1: 'Table de multiplication de 1',
     tableDe1Short: 'Table de 1',
+    tableDe2: 'Table de multiplication de 2',
+    tableDe2Short: 'Table de 2',
+    tableDe3: 'Table de multiplication de 3',
+    tableDe3Short: 'Table de 3',
+    tableDe4: 'Table de multiplication de 4',
+    tableDe4Short: 'Table de 4',
+    tableDe5: 'Table de multiplication de 5',
+    tableDe5Short: 'Table de 5',
+
+
   },
 }
 
