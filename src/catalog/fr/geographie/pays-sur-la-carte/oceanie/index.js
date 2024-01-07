1cce0f3fee05e905fa4cb209421f570d810ad2a5
Philippe Lucidarme <philippe@lucidar.me>
2024-01-07 18:49:16 +0100

    Add oceania countries on the map in French

---
+++ src/catalog/fr/geographie/pays-sur-la-carte/oceanie/index.js
@@ -0,0 +1,130 @@
+import australia from "Catalog/en/geography/countries-on-the-map/oceania/img/australia.png";
+import cookIslands from "Catalog/en/geography/countries-on-the-map/oceania/img/cook-islands.png";
+import fiji from "Catalog/en/geography/countries-on-the-map/oceania/img/fiji.png";
+import kiribati from "Catalog/en/geography/countries-on-the-map/oceania/img/kiribati.png";
+import marshallIslands from "Catalog/en/geography/countries-on-the-map/oceania/img/marshall-islands.png";
+import micronesia from "Catalog/en/geography/countries-on-the-map/oceania/img/micronesia.png";
+import nauru from "Catalog/en/geography/countries-on-the-map/oceania/img/nauru.png";
+import newZealand from "Catalog/en/geography/countries-on-the-map/oceania/img/new-zealand.png";
+import niue from "Catalog/en/geography/countries-on-the-map/oceania/img/niue.png";
+import palau from "Catalog/en/geography/countries-on-the-map/oceania/img/palau.png";
+import papuaNewGuinea from "Catalog/en/geography/countries-on-the-map/oceania/img/papua-new-guinea.png";
+import samoa from "Catalog/en/geography/countries-on-the-map/oceania/img/samoa.png";
+import solomonIslands from "Catalog/en/geography/countries-on-the-map/oceania/img/solomon-islands.png";
+import tonga from "Catalog/en/geography/countries-on-the-map/oceania/img/tonga.png";
+import tuvalu from "Catalog/en/geography/countries-on-the-map/oceania/img/tuvalu.png";
+import vanuatu from "Catalog/en/geography/countries-on-the-map/oceania/img/vanuatu.png";
+
+
+
+// Target language of the test
+export var metaData = {
+
+  // Quiz flag
+  flag: "/static/circle-flags/fr.svg",
+
+
+  // Special characters for this test
+  specialCharacters: ['é', 'à', 'è', 'ê', 'ô', 'û', 'â', 'î', 'ü', 'ù', 'ë', 'œ', 'ç', 'ï', 'É', 'À', 'È', 'Ê', 'Ô', 'Û', 'Â', 'Î', 'Ü', 'Ù', 'Ë', 'Œ', 'Ç', 'Ï'],
+
+}
+
+
+export var cards = {
+  "al": {
+    image: australia,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Australie"
+  },
+  "ck": {
+    image: cookIslands,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Îles Cook"
+  },
+  "fj": {
+    image: fiji,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Fidji"
+  },
+  "ki": {
+    image: kiribati,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Kiribati"
+  },
+  "mh": {
+    image: marshallIslands,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Îles Marshall"
+  },
+  "fm": {
+    image: micronesia,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Micronésie"
+  },
+  "nr": {
+    image: nauru,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Nauru"
+  },
+  "nz": {
+    image: newZealand,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Nouvelle-Zélande"
+  },
+  "nu": {
+    image: niue,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Niue"
+  },
+  "pw": {
+    image: palau,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Palaos"
+  },
+  "pg": {
+    image: papuaNewGuinea,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Papouasie-Nouvelle-Guinée"
+  },
+  "ws": {
+    image: samoa,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Samoa"
+  },
+  "sb": {
+    image: solomonIslands,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Îles Salomon"
+  },
+  "to": {
+    image: tonga,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Tonga"
+  },
+  "tv": {
+    image: tuvalu,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Tuvalu"
+  },
+  "vu": {
+    image: vanuatu,
+    type: 'outer',
+    prompt: "What country is this?",
+    answer: "Vanuatu"
+  },
+};
